41474fad421eeacb2cc9e31f83baed88d76aeded
Mugisha-Ronald <mugishacoder@gmail.com>
2024-03-02 13:18:26 +0300

    updated settings file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mine',
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
     
 ]
 
